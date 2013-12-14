4262aa43902ea439883bc40b19ec8560f778c3b0
Cristi Cobzarenco <cristi.cobzarenco@gmail.com>
2013-12-14 19:13:05 +0000

    switch swimmer texture

--- src/Settings.lua
+++ src/Settings.lua
@@ -2,14 +2,15 @@ settings = {
   world = {
     small_step_size = 1 / 120.0,
     screen_width = 10,
-    screen_pixel_width = 960,
-    screen_pixel_height = 540,
+    screen_pixel_width = 1280,
+    screen_pixel_height = 720,
     gravity = -0.02,
     global_drag = 0.165 * 20
   },
 
   debug = {
     show_lines = false,
+    disable_lightmap = true,
   },
 
   priorities = {
@@ -19,7 +20,7 @@ settings = {
 
   entities = {
     swimmer = {
-      texture_path = "assets/phone.png",
+      texture_path = "assets/swimmer64.png",
       size = 0.15,
       mass = 0.41,
       restitution = 0.2,
@@ -30,7 +31,7 @@ settings = {
 
   effects = {
     lightmap = {
-      light_texture_path = "assets/Spotlight-Map.png",
+      light_texture_path = "assets/spotlight-map.png",
       small_light_size = 1.0
     }
   },
